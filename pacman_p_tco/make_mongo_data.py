1d60c00b54d7aeefe209d35570187b9f869a63cb
yuuukuma <yuuukuma@gmail.com>
2022-09-23 18:38:23 +0900

    Update PTypeTcoData and GetPTypeTcoData

--- pacman_p_tco/make_mongo_data.py
+++ pacman_p_tco/make_mongo_data.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2022 Kumagai group.
+from dataclasses import dataclass
 from pathlib import Path
 from typing import List
 
-from monty.json import MontyDecoder
+import numpy as np
+from monty.json import MontyDecoder, MSONable
 from pacman.utils.get_database import get_database
+from pymatgen.core import Structure
+from pymatgen.io.vasp import Outcar
+from vise.analyzer.band_edge_properties import BandEdge, merge_band_edge
+from vise.analyzer.dielectric_function import DieleFuncData, \
+    min_e_w_target_coeff
 from vise.analyzer.effective_mass import EffectiveMass
 
 parent = Path(__file__).parent
@@ -20,13 +27,122 @@ def local_mongo():
     return get_database(config_file=parent / "local_mongo.json", admin=True)
 
 
-def get_effective_mass(formulas: List[str] = None):
-    result = {}
-    for doc in ato_mongo().effective_mass_dd_hybrid.find(
-            {"formula": {"$in": formulas}}):
-        em: EffectiveMass = pd(doc["effective_mass"])
-        ave_p_mass = em.average_mass("p", 10**18)
-        min_p_mass = em.minimum_mass("p", 10**18)
-        result[doc["formula"]] = (ave_p_mass, min_p_mass)
+@dataclass
+class PTypeTcoData(MSONable):
+    band_gap: float
+    optical_gap: float
+    ave_p_mass: float
+    ave_n_mass: float
+    min_p_mass: float
+    min_n_mass: float
+    vbm_band_edge: BandEdge
+    vbm_diff: float
+    cbm_band_edge: BandEdge
+    cbm_diff: float
+    oxygen_core_potentials: List[float]
 
-    return result
+    @property
+    def vbm_from_oxygen_core_potential(self):
+        ave_oxygen_potential = float(np.average(self.oxygen_core_potentials))
+        return self.vbm_band_edge.energy - ave_oxygen_potential
+
+    @property
+    def oxygen_core_potential_diff(self):
+        return (max(self.oxygen_core_potentials)
+                - min(self.oxygen_core_potentials))
+
+
+class GetPTypeTcoData:
+    def __init__(self, formulas: List[str] = None):
+        self.formulas = formulas
+        self.ato_mongo = ato_mongo()
+
+    def get_all_data(self):
+        result = {}
+        effective_masses = self.get_effective_masses()
+        optical_gaps = self.get_optical_gaps()
+        band_edges = self.get_band_edges()
+        oxygen_core_potentials = self.get_oxygen_core_potentials()
+
+        for k, effective_mass in effective_masses.items():
+            try:
+                result[k] = {**effective_mass,
+                             **optical_gaps[k],
+                             **oxygen_core_potentials[k],
+                             **band_edges[k]}
+            except KeyError:
+                print(f"{k} does not exist.")
+        return result
+
+    def get_effective_masses(self):
+        result = {}
+        for doc in self.ato_mongo.effective_mass_dd_hybrid.find(
+                {"formula": {"$in": self.formulas}}):
+            em: EffectiveMass = pd(doc["effective_mass"])
+            result[doc["formula"]] = \
+                {"ave_p_mass": em.average_mass("p", 10**18),
+                 "ave_n_mass": em.average_mass("n", 10**18),
+                 "min_p_mass": em.minimum_mass("p", 10**18),
+                 "min_n_mass": em.minimum_mass("n", 10**18)}
+        return result
+
+    def get_optical_gaps(self):
+        result = {}
+        for doc in self.ato_mongo.absorption_dd_hybrid.find(
+                {"formula": {"$in": self.formulas}},
+                {"formula": True, "diele_func": True}):
+            diele: DieleFuncData = pd(doc["diele_func"])
+            quantities = diele.absorption_coeff[-1]
+            opt_gap = min_e_w_target_coeff(diele.energies, quantities, 10**4)
+            result[doc["formula"]] = {"optical_gap": opt_gap}
+        return result
+
+    def get_band_edges(self):
+        result = {}
+        for doc in self.ato_mongo.absorption_dd_hybrid.find(
+                {"formula": {"$in": self.formulas}},
+                {"formula": True, "band_edge": True}):
+
+            abs_vbm: BandEdge = pd(doc["band_edge"]["vbm"])
+            abs_cbm: BandEdge = pd(doc["band_edge"]["cbm"])
+            abs_vbm.data_source = "absorption"
+            abs_cbm.data_source = "absorption"
+
+            band_doc = self.ato_mongo.band_dd_hybrid.find_one(
+                {"formula": doc["formula"]}, {"band_edge": True})
+            band_vbm: BandEdge = pd(band_doc["band_edge"]["vbm"])
+            band_cbm: BandEdge = pd(band_doc["band_edge"]["cbm"])
+
+            band_vbm.data_source = "band"
+            band_cbm.data_source = "band"
+
+            vbm = merge_band_edge(abs_vbm, band_vbm, "vbm")
+            cbm = merge_band_edge(abs_cbm, band_cbm, "cbm")
+
+            vbm_diff = round(abs(abs_vbm.energy - band_vbm.energy), 3)
+            cbm_diff = round(abs(abs_cbm.energy - band_cbm.energy), 3)
+            band_gap = cbm.energy - vbm.energy
+            result[doc["formula"]] = {"vbm_band_edge": vbm.as_dict(),
+                                      "cbm_band_edge": cbm.as_dict(),
+                                      "vbm_diff": vbm_diff,
+                                      "cbm_diff": cbm_diff,
+                                      "band_gap": band_gap}
+        return result
+
+    def get_oxygen_core_potentials(self):
+        result = {}
+        for doc in self.ato_mongo.absorption_dd_hybrid.find(
+                {"formula": {"$in": self.formulas}},
+                {"formula": True, "dirpath": True}):
+            path = Path(doc["dirpath"].replace(
+                "/storage", "/Users/kumagai/ato"))
+            structure = Structure.from_file(path / "CONTCAR-finish")
+            outcar = Outcar(path / "OUTCAR-finish")
+            o_site_potentials = []
+            for site, pot in zip(structure, outcar.electrostatic_potential):
+                if str(site.specie) == "O":
+                    o_site_potentials.append(pot)
+            result[doc["formula"]] = \
+                {"oxygen_core_potentials": o_site_potentials}
+
+        return result
--- pacman_p_tco/make_mongo_data_test.py
+++ pacman_p_tco/make_mongo_data_test.py
@@ -1,8 +1,88 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2022 Kumagai group.
-from pacman_p_tco.make_mongo_data import get_effective_mass
+import pytest
+from pymatgen.electronic_structure.core import Spin
+from vise.analyzer.band_edge_properties import BandEdge
+from vise.tests.helpers.assertion import assert_msonable
+
+from pacman_p_tco.make_mongo_data import GetPTypeTcoData, PTypeTcoData
+
+get_data = GetPTypeTcoData(formulas=["Al2O3"])
 
 
-def test_get_effective_mass():
-    actual = get_effective_mass(formulas=["MgO"])
-    assert actual == {"MgO": (2.097823279476968, 2.097823279476967)}
+@pytest.fixture
+def p_type_tco_data():
+    vbm_band_edge = BandEdge(energy=10.0,
+                             spin=Spin.up,
+                             band_index=1,
+                             kpoint_coords=[0.0, 0.0, 0.0],
+                             kpoint_index=1,
+                             data_source="band")
+    cbm_band_edge = BandEdge(energy=11.0,
+                             spin=Spin.up,
+                             band_index=2,
+                             kpoint_coords=[0.0, 0.0, 0.0],
+                             kpoint_index=1,
+                             data_source="dos")
+    return PTypeTcoData(band_gap=1.0,
+                        optical_gap=1.1,
+                        ave_p_mass=2.0,
+                        ave_n_mass=3.0,
+                        min_p_mass=1.9,
+                        min_n_mass=2.9,
+                        vbm_band_edge=vbm_band_edge,
+                        vbm_diff=0.001,
+                        cbm_band_edge=cbm_band_edge,
+                        cbm_diff=0.001,
+                        oxygen_core_potentials=[-10.0, -15.0, -20.0])
+
+
+def test_p_type_tco_data_msonable(p_type_tco_data):
+    assert_msonable(p_type_tco_data)
+
+
+def test_p_type_tco_vbm_from_oxygen_core_potential(p_type_tco_data):
+    actual = p_type_tco_data.vbm_from_oxygen_core_potential
+    vbm_energy, ave_oxygen_potential = 10.0, -15.0
+    expected = vbm_energy - ave_oxygen_potential
+    assert actual == expected
+
+
+def test_p_type_tco_oxygen_potential_core_potential_diff(p_type_tco_data):
+    actual = p_type_tco_data.oxygen_core_potential_diff
+    assert actual == 10.0
+
+
+def test_get_all_data():
+    actual = get_data.get_all_data()
+    expected = {"Al2O3": {"ave_p_mass": 4.152470711508106,
+                          "ave_n_mass": 0.37259405914217364,
+                          "min_p_mass": 2.1745967142009572,
+                          "min_n_mass": 0.3611766720422496,
+                          "optical_gap": 9.5176,
+                          "oxygen_core_potentials": [-59.1305]*6,
+                          'cbm_band_edge': {
+                              '@class': 'BandEdge',
+                              '@module': 'vise.analyzer.band_edge_properties',
+                              'band_index': 24,
+                              'data_source': 'absorption band',
+                              'energy': 13.3062,
+                              'kpoint_coords': [0.0, 0.0, 0.0],
+                              'kpoint_index': 0,
+                              'spin': 1,
+                              'symbol': None},
+                          'cbm_diff': 0.0,
+                          'vbm_band_edge': {
+                               '@class': 'BandEdge',
+                               '@module': 'vise.analyzer.band_edge_properties',
+                               'band_index': 23,
+                               'data_source': 'absorption band',
+                               'energy': 3.9742,
+                               'kpoint_coords': [0.0, 0.0, 0.0],
+                               'kpoint_index': 0,
+                               'spin': 1,
+                               'symbol': None},
+                          'vbm_diff': 0.001,
+                          "band_gap": 9.332}}
+    assert actual == expected
+
